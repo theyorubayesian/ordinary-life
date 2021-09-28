782a219e97900daeccd70fc7844f0aa03688ceac
Akintunde Oladipo <akin.o.oladipo@gmail.com>
2021-09-28 23:09:18 +0100

    complete training logic

---
+++ main.py
@@ -0,0 +1,365 @@
+import argparse
+import json
+import logging
+import os
+import time
+from math import ceil
+
+import torch
+import neptune.new as neptune
+from dotenv import load_dotenv
+from google.cloud import storage
+from torch.optim import AdamW
+from transformers import get_linear_schedule_with_warmup
+from transformers import GPT2Config
+from transformers import GPT2Tokenizer
+from transformers import GPT2LMHeadModel
+
+from train import prepare_training_data
+from train import train_epoch
+from utils import init_gpu_params
+from utils import set_seed
+from utils import Iters
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - PID: %(process)d -  %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+
+
+def get_model_and_tokenizer(args):
+    tokenizer = GPT2Tokenizer.from_pretrained(args.tokenizer_name)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    if args.special_tokens: # TODO: Check
+        assert isinstance(args.special_tokens, dict), "Check that special_tokens is dict"
+        tokenizer.add_special_tokens(args.special_tokens)
+
+    config = GPT2Config.from_pretrained(
+        args.config_name,
+        cache_dir=args.cache_dir,
+        output_hidden_states=args.output_hidden_states
+    )
+    model = GPT2LMHeadModel.from_pretrained(
+        args.model_name_or_path,
+        cache_dir=args.cache_dir,
+        config=config
+    )
+    model.resize_token_embeddings(len(tokenizer))
+    
+    return model, tokenizer
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    
+    # -----------------
+    # Model & Tokenizer
+    # -----------------
+    parser.add_argument(
+        "--tokenizer_name"
+    )
+    parser.add_argument(
+        "--config_name"
+    )
+    parser.add_argument(
+        "--model_name_or_path"
+    )
+    parser.add_argument(
+        "--special_tokens",
+        type=json.loads,
+    )
+
+    # ----
+    # Data
+    # ----
+    parser.add_argument(
+        "--dataset_name"
+    )
+    parser.add_argument(
+        "--max_seq_len",
+        default=128,
+        type=int
+    )
+    parser.add_argument(
+        "-min_seq_len",
+        default=7,
+        type=int
+    )
+    parser.add_argument(
+        "--overwrite_cache",
+        action="store_true"
+    )
+    parser.add_argument(
+        "--cache_dir"
+    )
+
+    # ---------
+    # Modelling
+    # ---------
+    parser.add_argument(
+        "--fp16",
+        action="store_true"
+    )
+    parser.add_argument(
+        "--fp16_opt_level",
+        type=str,
+        default="O1"
+    )
+    parser.add_argument(
+        "--continue_from_step",
+        default=0,
+        help="Step at which training should begin"
+    )
+    parser.add_argument(
+        "--train_batch_size",
+        default=8,
+        type=int
+    )
+    parser.add_argument(
+        "--val_batch_size",
+        default=8,
+        type=int
+    )
+    parser.add_argument(
+        "--n_epochs",
+        type=int,
+        default=5,
+        help="Number of epochs to train for"
+    )
+    parser.add_argument(
+        "--num_training_steps",
+        type=int,
+        default=10000
+    )
+    parser.add_argument(
+        "--warmup_steps",
+        type=int,
+        default=4000
+    )
+    parser.add_argument(
+        "--learning_rate",
+        type=float,
+        default=1e-5
+    )
+    parser.add_argument(
+        "--adam_epsilon",
+        default=1e-6,
+    )
+    parser.add_argument(
+        "--max_grad_norm",
+        default=1.0,
+        type=float
+    )
+    parser.add_argument(
+        "--weight_decay",
+        default=0.01,
+        type=float
+    )
+    parser.add_argument(
+        "--gradient_accumulation_steps",
+        default=1,
+        help="" # TODO
+    )
+
+    # -----------------------
+    # Validation & Checkpoint
+    # -----------------------
+    parser.add_argument(
+        "--checkpoint_interval",
+        default=5000,
+        type=int
+    )
+    parser.add_argument(
+        "--validation_interval",
+        default=5000,
+        type=int
+    )
+    parser.add_argument(
+        "--num_val_checkpoints",
+        default=5,
+        type=int,
+        help="Number of Validation checkpoints to keep"
+    )
+    parser.add_argument(
+        "--generate_responses",
+        action="store_true"
+    )
+    parser.add_argument(
+        "--upload_to_bucket",
+        action="store_true"
+    )
+    parser.add_argument(
+        "--bucket_dir",
+        type=str,
+        help=""
+    )
+    parser.add_argument(
+        "--cleanup",
+        action="store_true"
+    )
+
+    # ---
+    # GPU
+    # ---
+    parser.add_argument(
+        "--n_gpu",
+        default=0,  # TODO
+        type=int
+    )
+    parser.add_argument(
+        "--local_rank",
+        default=-1,
+        type=int
+    )
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    # --------------
+    # Initialization
+    # --------------
+    args = get_parser()
+    # TODO
+    # run_sanity_check(args)
+    init_gpu_params(args)
+    set_seed(args)
+    args.device = torch.device(f"cuda:{args.local_rank}" if args.n_gpu else "cpu")
+
+    model, tokenizer = get_model_and_tokenizer(args)
+    train_dataloader, val_dataloader = prepare_training_data(args, tokenizer)
+
+    if args.num_training_steps:
+        num_training_steps = args.num_training_steps
+        args.n_epochs = ceil(args.num_training_steps / len(train_dataloader) / args.gradient_accumulation_steps)
+    else:
+        num_training_steps = len(train_dataloader) // args.gradient_accumulation_steps * args.n_epochs
+    
+    # -----------------
+    # Logging & Storage
+    # -----------------
+    logging_client = None
+    storage_client = None
+
+    if args.is_master:
+        logging_client = neptune.init(
+            project=os.getenv("NEPTUNE_PROJECT_NAME"),
+            api_token=os.getenv("NEPTUNE_API_TOKEN"),
+            mode=os.getenv("NEPTUNE_CONNECTION_MODE"),
+            source_files=[] # TODO
+        )
+        args_copy = vars(args)
+        for a in args_copy:
+            logging_client[a] = args_copy[a]
+            logger.info(f"{a}:  {args_copy[a]}")
+
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(
+            os.getenv("GOOGLE_CLOUD_BUCKET_NAME")
+        )
+    
+    # --------
+    # Trackers
+    # --------
+    iters = Iters(
+        {
+            "days":  0,
+            "epoch": 0,
+            "steps_trained_in_epoch": 0,
+            "epoch_step": 0,
+            "global_step": 0,
+            "step": 0,
+            "total_training_time": 0,
+            "last_loss": 0,
+            "last_ppl": 0,
+            "total_loss_epoch": 0,
+            "n_sequences_epoch": 0,
+            "last_log": time.time(),
+            "last_iter_time": time.time(),
+            "validation_ckpts": []
+        }
+    )
+
+    # ------------------------
+    # Optimizer Initialization
+    # ------------------------
+    no_decay = ["bias", "ln"]
+    optimizer_grouped_parameters = [
+        {
+            "params": [
+                p for n, p in model.named_parameters()
+                if not any(nd in n for nd in no_decay) and p.requires_grad
+            ],
+            "weight_decay": args.weight_decay
+        },
+        {
+            "params": [
+                p for n, p in model.named_parameters()
+                if any(nd in n for nd in no_decay) and p.requires_grad
+            ],
+            "weight_decay": 0.0
+        }
+    ]
+    optimizer = AdamW(
+        optimizer_grouped_parameters,
+        lr=args.learning_rate,
+        eps=args.adam_epsilon,
+        betas=(0.9, 0.999),
+    )
+    optimizer_dump_path = os.path.join(args.output_dir, "optimizer.pt")
+
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer,
+        num_warmup_steps=args.warmup_steps,
+        num_training_steps=num_training_steps
+    )
+    scheduler_dump_path = os.path.join(args.output_dir, "scheduler.pt")
+
+    if (
+        args.model_name_or_path and
+        os.path.isfile(optimizer_dump_path) and
+        os.path.isfile(scheduler_dump_path)
+    ):
+        optimizer.load_state_dict(torch.load(optimizer_dump_path))
+        scheduler.load_state_dict(torch.load(scheduler_dump_path))    
+
+    if args.fp16:
+        try:
+            from apex import amp
+        except ImportError:
+            raise ImportError("Please install apex: https://www.github.com/nvidia/apex"
+            " for fp16 training")
+        model, optimizer = amp.initialize(model, optimizer, opt_level=args.fp16_op_level)
+    
+    # --------
+    # Training
+    # --------
+    if args.continue_from_step:
+        assert os.path.exists(args.model_name_or_path)
+        iters.global_step = int(args.model_name_or_path.split("-")[-1])
+        iters.epoch = iters.global_step // (len(train_dataloader) // args.gradient_accumulation_steps)
+        iters.steps_trained_in_epoch = iters.global_step % (len(train_dataloader) // args.gradient_accumulation_steps)
+
+    while iters.epoch < args.n_epochs:
+        if args.multi_gpu:
+            torch.distributed.barrier()
+
+        train_epoch(
+            train_dataloader=train_dataloader,
+            val_dataloader=val_dataloader,
+            model=model,
+            tokenizer=tokenizer,
+            optimizer=optimizer,
+            iters=iters,
+            logging_client=logging_client,
+            bucket=bucket,
+        )
+
+
+if __name__ == "__main__":
+    main()
---
+++ train.py
@@ -0,0 +1,339 @@
+import logging
+import os
+import shutil
+import time
+from bisect import bisect_left
+from math import exp
+
+import numpy as np
+import psutil
+import torch
+from datasets import load_dataset
+from torch import nn
+
+from dataset import DialogDataset
+from dataset import DialogDataLoader
+# from generate import generate_responses
+
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - PID: %(process)d -  %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
+
+
+def prepare_training_data(args, tokenizer):
+    data = load_dataset(args.dataset_name)
+    train, val = data["train"], data["validation"]
+    train_data = DialogDataset(tokenizer, train["dialog"], "train", args)
+    val_data = DialogDataset(tokenizer, val["dialog"], "val", args)
+
+    # train_dataloader = DataLoader(train_data, batch_size=args.train_batch_size, shuffle=True)
+    # val_dataloader = DataLoader(val_data, args.val_batch_size, shuffle=False)
+
+    train_dataloader = DialogDataLoader(
+        data=train_data,
+        batch_size=args.train_batch_size,
+        dtype=torch.long,
+        repeat=True,
+        shuffle=True,
+        sort=False,
+        sort_within_batch=True,
+        args=args
+    )
+    val_dataloader = DialogDataLoader(
+        data=val_data,
+        batch_size=args.val_batch_size,
+        dtype=torch.long,
+        repeat=True,
+        shuffle=True,
+        sort=False,
+        sort_within_batch=True,
+        args=args
+    )
+
+    return train_dataloader, val_dataloader
+
+
+def log_neptune(logging_client, model, optimizer, iters, log_params=False):
+    logging_client["losses/cum_avg_loss_epoch"].log(
+        iters.total_loss_epoch / iters.epoch_step)
+    logging_client["losses/loss"].log(iters.last_loss)
+    logging_client["metric/ppl"].log(iters.last_ppl)
+    
+    if log_params:
+        for param_name, param in model.named_parameters():
+            logging_client[f"parameter_mean/{param_name}"].log(
+                param.data.mean())
+            logging_client[f"parameter_std/{param_name}"].log(
+                param.data.std())
+            if param.grad is None:
+                continue
+            logging_client[f"grad_mean/{param_name}"].log(
+                param.grad.data.mean())
+            logging_client[f"grad_std/{param_name}"].log(
+                param.grad.data.std())
+
+    logging_client["global/lr"].log(optimizer.param_groups[0]["lr"])
+    logging_client["global/memory_usage"].log(psutil.virtual_memory()._asdict()["used"] / 1_000_000)
+    logging_client["global/speed"].log(time.time() - iters.last_log)
+
+
+def save_checkpoint(
+    model,
+    optimizer,
+    scheduler,
+    output_dir,
+    to_bucket=False,
+    bucket=None,
+    bucket_dir=None,
+    cleanup=True
+):
+    os.makedirs(output_dir, exist_ok=True)
+
+    model_to_save = model.module if hasattr(model, "module") else model
+    model_to_save.save_pretrained(output_dir)
+    # tokenizer.save_pretrained(output_dir)
+    torch.save(
+        optimizer.state_dict(), os.path.join(output_dir, "optimizer.pt")
+    )
+    torch.save(
+        scheduler.state_dict(), os.path.join(output_dir, "scheduler.pt")
+    )
+    logger.info(f"Saving model, optimizer and scheduler states to {output_dir}")
+    
+    if to_bucket:
+        for f in os.listdir(output_dir):
+            dest = f"{bucket_dir}/{f}"
+            blob = bucket.blob(dest)
+            blob.upload_from_filename(os.path.join(output_dir, f))
+
+        logger.info(f"Uploaded checkpoints to bucket: {bucket_dir}")
+        if cleanup:
+            shutil.rmtree(output_dir)
+
+
+def validate(
+    model,
+    dataloader,
+    iters,
+    args
+):
+    model.eval()
+    tot_loss = []
+    tot_ppl = []
+    tot_sample = []
+
+    with torch.no_grad():
+        for batch in dataloader:
+            if args.n_gpu > 0:
+                batch = tuple(t.to(args.device) for t in batch)
+            input_ids, label_ids = batch
+            n_sample = input_ids.shape[0]
+
+            outputs = model(
+                input_ids=input_ids,
+                labels=label_ids,
+                return_dict=True
+            )
+            loss = outputs.loss
+            ppl = exp(loss)
+
+            tot_loss.append(loss.mean().item() * n_sample)
+            tot_ppl.append(ppl.mean().item() * n_sample)
+            tot_sample.append(n_sample)
+    val_loss = np.sum(tot_loss) / np.sum(tot_sample)
+    val_ppl = np.sum(tot_ppl) / np.sum(tot_sample)
+    print(
+        f"Epoch {iters.epoch}: \nVal loss: {val_loss}\n" \
+        f"Val ppl: {val_ppl}"
+    )
+    return val_loss, val_ppl
+
+
+def check_val_rank(existing_ckpts, ppl, args):
+    ckpt_rank = bisect_left([v[1] for v in existing_ckpts], ppl)
+
+    if (ckpt_rank < len(existing_ckpts)) or (len(existing_ckpts) < args.num_val_checkpoints):
+        return ckpt_rank
+    return -1
+
+
+def train_epoch(
+    train_dataloader, 
+    val_dataloader,
+    model, 
+    tokenizer,
+    optimizer,
+    scheduler,
+    iters,
+    logging_client,
+    bucket,
+    args
+):
+    for batch in train_dataloader:
+        if iters.steps_trained_in_epoch > 0:
+            iters.step_trained_in_epoch -= 1
+            continue
+
+        if args.n_gpu > 0:
+            batch = tuple(t.to(args.device) for t in batch)
+        input_ids, label_ids  = batch
+
+        outputs = model(
+            input_ids=input_ids,
+            labels=label_ids,
+            return_dict=True
+        )
+        loss= outputs.loss
+
+        if (loss != loss).data.any():
+            logger.error("NaN detected in loss")
+            exit()
+        
+        if args.multi_gpu:
+            loss = loss.mean()
+        
+        if args.gradient_accumulation_steps > 1:
+            loss = loss / args.gradient_accumulation_steps
+        
+        ppl = exp(loss.item())
+        loss.backward()
+        
+        iters.last_ppl = ppl
+        iters.last_loss = loss.item()
+        iters.total_loss_epoch += loss.item()
+        iters.step += 1
+        iters.epoch_step += 1
+        iters.total_training_time += time.time() - iters.last_iter_time
+        iters.last_iter_time = time.time()
+        
+        if iters.step % args.gradient_accumulation_steps == 0:
+            iters.global_step += 1
+            nn.utils.clip_grad_norm_(
+                model.parameters(), args.max_grad_norm
+            )
+
+            optimizer.step()
+            scheduler.step()
+
+            if args.is_master:
+                log_neptune(logging_client, model, optimizer, iters, log_params=True)
+                iters.last_log = time.time()
+            
+            optimizer.zero_grad()
+            
+            if args.is_master:
+                if iters.global_step % args.checkpoint_interval == 0:
+                    output_dir = os.path.join(
+                        args.output_dir, f"checkpoint-{iters.global_step}")
+                    save_checkpoint(
+                        model=model,
+                        optimizer=optimizer,
+                        scheduler=scheduler,
+                        output_dir=output_dir,
+                        upload_to_bucket=args.upload_to_bucket,
+                        bucket=bucket,
+                        bucket_dir=args.bucket_dir,
+                        cleanup=args.cleanup
+                    )
+
+                if iters.global_step % args.validation_interval == 0:
+                    val_loss, val_ppl = validate(
+                        model=model,
+                        dataloader=val_dataloader,
+                        iters=iters,
+                        args=args
+                    )
+                    logging_client["val/loss"].log(val_loss)
+                    logging_client["val/ppl"].log(val_ppl)
+
+                    # TODO
+                    checkpoint_val_rank = check_val_rank(iters.validation_ckpts, val_ppl, args)
+
+                    # TODO: Checkpoint during validation?
+                    if (checkpoint_val_rank  != -1):
+                        if args.validation_interval != args.checkpoint_interval:
+                            output_dir = os.path.join(
+                                args.output_dir, f"{CHECKPOINT_PREFIX}-validation-{iters.global_step}")
+                            save_checkpoint(
+                                model=model,
+                                optimizer=optimizer,
+                                scheduler=scheduler,
+                                output_dir=args.output_dir,
+                                upload_to_bucket=args.upload_to_bucket,
+                                bucket=bucket,
+                                bucket_dir=args.bucket_dir,
+                                cleanup=args.cleanup
+                            )
+                        else:
+                            os.rename(output_dir, "-validation-".join(output_dir.split("-")))
+
+        iters.n_sequences_epoch += input_ids.size(0)
+    # iters.n_toten_total += input_ids.shape[0] * input_ids.shape[1]
+    # iters.n_token_real += (input_ids != 0).sum().item()
+
+    if args.is_master:
+        logger.info(f"Ending epoch {iters.epoch}/{args.n_epochs-1}")
+        output_dir = os.path.join(
+            args.output_dir, f"epoch_checkpoint-{iters.epoch}"
+        )
+        save_checkpoint(
+            model=model,
+            optimizer=optimizer,
+            scheduler=scheduler,
+            output_dir=args.output_dir,
+            upload_to_bucket=args.upload_to_bucket,
+            bucket=bucket,
+            bucket_dir=args.bucket_dir,
+            cleanup=args.cleanup
+        )
+        logging_client["epoch/loss"].log(iters.total_loss_epoch / iters.epoch_step)
+
+        if args.generate_responses:
+            # model.eval()
+
+            # TODO
+            # generate_responses(
+            #    model=model,
+            #    tokenizer=tokenizer,
+            #    batch_size=64,
+            #    tokenizer_max_len=128,
+            #    model_max_len=128,
+            #    beam=1,
+            #    context_file="compression/distillation/msft/dstc/data/test.source",
+            #    output_file=f"{args.dump_path}/model.epoch_{iters.epoch}.6k.resp.txt"
+            # )   # TODO
+            
+            # model.train()
+            pass
+        
+        iters.epoch += 1
+        iters.epoch_step = 0
+        iters.n_sequences_epoch = 0
+        iters.total_loss_epoch = 0
+    
+    if args.is_master:
+        val_loss, val_ppl = validate(
+            model=model,
+            dataloader=val_dataloader,
+            iters=iters,
+            args=args
+        )
+        logging_client["val/loss"].log(val_loss)
+        logging_client["val/ppl"].log(val_ppl)
+
+        logger.info("Saving final checkpoint as `pytorch_model.bin`")
+        save_checkpoint(
+                    model=model,
+                    optimizer=optimizer,
+                    scheduler=scheduler,
+                    output_dir=args.output_dir,
+                    upload_to_bucket=args.upload_to_bucket,
+                    bucket=bucket,
+                    bucket_dir=args.bucket_dir,
+                    cleanup=args.cleanup
+                )        
+        logger.info("Training is finished")
