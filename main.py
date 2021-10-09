5b03ceb07edb2f21e48f2ea122083370c04edc8c
theyorubayesian <akin.o.oladipo@gmail.com>
2021-10-09 22:52:59 +0000

    use HfArgumentParser not argparse

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import argparse
 import json
 import logging
 
@@ -6,6 +5,7 @@ import torch
 from transformers import GPT2Config
 from transformers import GPT2Tokenizer
 from transformers import GPT2LMHeadModel
+from transformers import HfArgumentParser
 
 from train import prepare_training_data
 from trainer import DailyDialogTrainer as Trainer
@@ -45,8 +45,8 @@ def get_model_and_tokenizer(args):
 
 
 def get_parser():
-    parser = argparse.ArgumentParser()
-    
+    # parser = argparse.ArgumentParser()
+    parser = HfArgumentParser(TrainingArguments)
     # -----------------
     # Model & Tokenizer
     # -----------------
@@ -88,142 +88,9 @@ def get_parser():
         "--cache_dir"
     )
 
-    # ---------
-    # Modelling
-    # ---------
-    parser.add_argument(
-        "--do_train",
-        action="store_true",
-        help="Whether to run training or not"
-    )
-    parser.add_argument(
-        "--do_eval",
-        action="store_true",
-        help="Whether to run evaluation on validation set or not"
-    )
-    parser.add_argument(
-        "--evaluation_strategy",
-        default="steps",
-        help="One of no|steps|epoch"
-    )
-    parser.add_argument(
-        "--logging_dir",
-        default="logs"
-    )
-    parser.add_argument(
-        "--logging_strategy",
-        default="steps"
-    )
-    parser.add_argument(
-        "--logging_steps",
-        tupe=int,
-        help="Number of update steps between logs"
-    )
-    parser.add_argument(
-        "--save_strategy"
-    )
-    parser.add_argument(
-        "--output_dir",
-        default="outputs"
-    )
-    parser.add_argument(
-        "--seed",
-        default=42
-    )
-    parser.add_argument(
-        "--fp16",
-        action="store_true"
-    )
-    parser.add_argument(
-        "--fp16_opt_level",
-        type=str,
-        default="O1"
-    )
-    parser.add_argument(
-        "--continue_from_step",
-        default=0,
-        help="Step at which training should begin"
-    )
-    parser.add_argument(
-        "--per_device_train_batch_size",
-        default=8,
-        type=int
-    )
-    parser.add_argument(
-        "--per_device_eval_batch_size",
-        default=8,
-        type=int
-    )
-    parser.add_argument(
-        "--gradient_accumulation_steps",
-        default=1,
-        type=int,
-        help="" # TODO
-    )
-    parser.add_argument(
-        "--n_epochs",
-        type=int,
-        default=5,
-        help="Number of epochs to train for"
-    )
-    parser.add_argument(
-        "--num_training_steps",
-        type=int,
-        default=10000
-    )
-    parser.add_argument(
-        "--warmup_steps",
-        type=int,
-        default=4000
-    )
-    parser.add_argument(
-        "--learning_rate",
-        type=float,
-        default=1e-5
-    )
-    parser.add_argument(
-        "--lr_scheduler_type",
-        type=str,
-        help=
-    )
-    parser.add_argument(
-        "--adam_epsilon",
-        default=1e-6,
-    )
-    parser.add_argument(
-        "--max_grad_norm",
-        default=1.0,
-        type=float
-    )
-    parser.add_argument(
-        "--weight_decay",
-        default=0.01,
-        type=float
-    )
-
     # -----------------------
     # Validation & Checkpoint
     # -----------------------
-    parser.add_argument(
-        "--log_params",
-        action="store_true"
-    )
-    parser.add_argument(
-        "--checkpoint_interval",
-        default=5000,
-        type=int
-    )
-    parser.add_argument(
-        "--validation_interval",
-        default=5000,
-        type=int
-    )
-    parser.add_argument(
-        "--num_val_checkpoints",
-        default=5,
-        type=int,
-        help="Number of Validation checkpoints to keep"
-    )
     parser.add_argument(
         "--generate_responses",
         action="store_true"
@@ -242,62 +109,49 @@ def get_parser():
         action="store_true"
     )
 
-    # ---
-    # GPU
-    # ---
-    parser.add_argument(
-        "--n_gpu",
-        default=0,  # TODO
-        type=int
-    )
-    parser.add_argument(
-        "--local_rank",
-        default=-1,
-        type=int
-    )
-    args = parser.parse_args()
-    return args
+    training_args, init_args = parser.parse_args_into_dataclasses()
+    return training_args, init_args
 
 
 def main():
     # --------------
     # Initialization
     # --------------
-    args = get_parser()
+    training_args, init_args = get_parser()
     # TODO
     # run_sanity_check(args)
-    init_gpu_params(args)
-    set_seed(args)
-    args.device = torch.device(f"cuda:{args.local_rank}" if args.n_gpu else "cpu")
+    # init_gpu_params(args) # TODO
+    # set_seed(args)    # TODO
+    training_args.device = torch.device(f"cuda:{training_args.local_rank}" if training_args.n_gpu else "cpu")
 
-    model, tokenizer = get_model_and_tokenizer(args)
+    model, tokenizer = get_model_and_tokenizer(init_args)
 
-    train_dataset, val_dataset = prepare_training_data(args, tokenizer)
+    train_dataset, val_dataset = prepare_training_data(init_args, tokenizer)
 
     # TODO: Include no_cuda argument 
-    training_args = TrainingArguments(
-        output_dir=args.output_dir,
-        report_to="neptune",
-        sampler_bucket_size=100,
-        dataloader_drop_last=True,
-        dataloader_shuffle=True,
-        # dataloader_pin_memory=False,
-        # dataloader_num_workers=1,
-        shuffle=True,
-        num_train_epochs=args.n_epochs,
-        per_device_train_batch_size=args.train_batch_size,
-        per_device_eval_batch_size=args.val_batch_size,
-        evaluation_strategy="epoch",
-        fp16=args.fp16,
-        fp16_opt_level=args.fp16_opt_level,
-        warmup_steps=args.warmup_steps,
-        learning_rate=args.learning_rate,
-        lr_scheduler_type=args.lr_scheduler_type,
-        adam_epsilon=args.adam_epsilon,
-        weight_decay=args.weight_decay,
-        save_total_limit=3, # TODO
-        load_best_model_at_end=True # TODO
-    )
+    # training_args = TrainingArguments(
+    #    output_dir=args.output_dir,
+    #    report_to="neptune",
+    #    sampler_bucket_size=100,
+    #    dataloader_drop_last=True,
+    #    dataloader_shuffle=True,
+    #    # dataloader_pin_memory=False,
+    #    # dataloader_num_workers=1,
+    #    shuffle=True,
+    #    num_train_epochs=args.n_epochs,
+    #    per_device_train_batch_size=args.train_batch_size,
+    #    per_device_eval_batch_size=args.val_batch_size,
+    #    evaluation_strategy="epoch",
+    #    fp16=args.fp16,
+    #    fp16_opt_level=args.fp16_opt_level,
+    #    warmup_steps=args.warmup_steps,
+    #    learning_rate=args.learning_rate,
+    #    lr_scheduler_type=args.lr_scheduler_type,
+    #    adam_epsilon=args.adam_epsilon,
+    #    weight_decay=args.weight_decay,
+    #    save_total_limit=3, # TODO
+    #    load_best_model_at_end=True # TODO
+    # )
 
     trainer = Trainer(
         model=model,
